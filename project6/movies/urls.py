5c517463da9a74f9f4f9cbc8a9cc010e25674a71
Vladimír Hanhur <aqgz@email.cz>
2024-11-13 15:27:43 +0100

    update

---
+++ project6/movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.start, name = 'starting-page'),
+    path('all-movies/', views.all_movies, name = 'all-movies-page'),
+    path('all-movies/<slug:slug>', views.movie_detail, name = 'movie-detail-page'),
+]
