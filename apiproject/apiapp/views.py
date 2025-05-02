95015741d1b93419e717072a0d6df8cc2368af59
Vladimír Hanhur <aqgz@email.cz>
2025-05-02 20:10:03 +0200

    update

---
+++ apiproject/apiapp/views.py
@@ -0,0 +1,17 @@
+# from django.shortcuts import render
+import json
+import os
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['GET'])
+def api(request):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    file_path = os.path.join(base_dir, 'data.json')
+
+    with open(file_path, encoding = 'utf-8') as file:
+        data = json.load(file)
+
+    return Response(data)
